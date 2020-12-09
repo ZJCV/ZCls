092d6fab6a743f599bb5e53d3df2c886d54b61c4
zjkjzj <wy163zhuj@163.com>
2020-12-09 18:39:42 +0800

    improvement(config): 更新MEAN和STD参数

--- zcls/config/transform.py
+++ zcls/config/transform.py
@@ -7,7 +7,6 @@
 @description: 
 """
 
-
 from yacs.config import CfgNode as CN
 
 
@@ -16,8 +15,8 @@ def add_config(_C):
     # Transform
     # ---------------------------------------------------------------------------- #
     _C.TRANSFORM = CN()
-    _C.TRANSFORM.MEAN = (0.5071, 0.4865, 0.4409)
-    _C.TRANSFORM.STD = (0.1942, 0.1918, 0.1958)
+    _C.TRANSFORM.MEAN = (0.45, 0.45, 0.45)
+    _C.TRANSFORM.STD = (0.225, 0.225, 0.225)
 
     _C.TRANSFORM.TRAIN = CN()
     _C.TRANSFORM.TRAIN.SHORTER_SIDE = 224
