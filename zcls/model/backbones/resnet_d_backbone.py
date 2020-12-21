2b2c1f23042b2779ddd78aa82c4579d9231f41c2
zjkjzj <wy163zhuj@163.com>
2020-12-21 21:52:11 +0800

    feat(model): 新建ResNetDRecognizer

---
+++ zcls/model/backbones/resnet_d_backbone.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/11/21 下午7:04
+@file: resnet_backbone.py
+@author: zj
+@description: 
+"""
+
+import torch.nn as nn
+
+from ..backbones.basicblock import BasicBlock
+from ..backbones.bottleneck import Bottleneck
+
+
+class ResNetDBackbone(nn.Module):
+    """
+    参考：
+    [Bag of Tricks for Image Classification with Convolutional Neural Networks](https://arxiv.org/abs/1812.01187)
+    """
+
+    def __init__(self,
+                 # 输入通道数
+                 inplanes=3,
+                 # 基础通道数,
+                 base_planes=64,
+                 # 每一层通道数
+                 layer_planes=(64, 128, 256, 512),
+                 # 每一层块个数
+                 layer_blocks=(2, 2, 2, 2),
+                 # 是否执行空间下采样
+                 downsamples=(0, 1, 1, 1),
+                 # 块类型
+                 block_layer=None,
+                 # 卷积层类型
+                 conv_layer=None,
+                 # 归一化层类型
+                 norm_layer=None,
+                 # 激活层类型
+                 act_layer=None,
+                 # 零初始化残差连接
+                 zero_init_residual=False
+                 ):
+        super(ResNetDBackbone, self).__init__()
+
+        if block_layer is None:
+            block_layer = BasicBlock
+        if conv_layer is None:
+            conv_layer = nn.Conv2d
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if act_layer is None:
+            act_layer = nn.ReLU
+
+        self.inplanes = inplanes
+        self.base_planes = base_planes
+        self.layer_planes = layer_planes
+        self.layer_blocks = layer_blocks
+        self.downsamples = downsamples
+        self.block_layer = block_layer
+        self.conv_layer = conv_layer
+        self.norm_layer = norm_layer
+        self.act_layer = act_layer
+        self.zero_init_residual = zero_init_residual
+
+        self._make_stem()
+        self.inplanes = self.base_planes
+        for i in range(len(self.layer_blocks)):
+            res_layer = self._make_res_layer(self.inplanes,
+                                             self.layer_planes[i],
+                                             self.layer_blocks[i],
+                                             self.downsamples[i],
+                                             self.block_layer,
+                                             self.conv_layer,
+                                             self.norm_layer,
+                                             self.act_layer
+                                             )
+            self.inplanes = self.layer_planes[i] * self.block_layer.expansion
+            layer_name = f'layer{i + 1}'
+            self.add_module(layer_name, res_layer)
+
+        self._init_weights(self.zero_init_residual)
+
+    def _make_stem(self):
+        inner_planes = self.base_planes // 2
+        self.conv1 = self.conv_layer(self.inplanes, inner_planes, kernel_size=3, stride=2, padding=1,
+                                     bias=False)
+        self.bn1 = self.norm_layer(inner_planes)
+        self.conv2 = self.conv_layer(inner_planes, inner_planes, kernel_size=3, stride=1, padding=1,
+                                     bias=False)
+        self.bn2 = self.norm_layer(inner_planes)
+        self.conv3 = self.conv_layer(inner_planes, self.base_planes, kernel_size=3, stride=1, padding=1,
+                                     bias=False)
+        self.bn3 = self.norm_layer(self.base_planes)
+        self.relu = self.act_layer(inplace=True)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+    def _make_res_layer(self,
+                        # 输入通道数
+                        inplanes,
+                        # 输出通道数
+                        planes,
+                        # 块个数
+                        block_num,
+                        # 是否执行空间下采样
+                        is_downsample,
+                        # 块类型
+                        block_layer,
+                        # 卷积层类型
+                        conv_layer,
+                        # 归一化层类型
+                        norm_layer,
+                        # 激活层类型
+                        act_layer,
+                        ):
+        stride = 2 if is_downsample else 1
+        expansion = self.block_layer.expansion
+        if is_downsample:
+            downsample = nn.Sequential(
+                nn.AvgPool2d(kernel_size=2, stride=2),
+                conv_layer(inplanes, planes * expansion, kernel_size=1, stride=1, bias=False),
+                norm_layer(planes * expansion),
+            )
+        elif inplanes != planes * expansion:
+            downsample = nn.Sequential(
+                conv_layer(inplanes, planes * expansion, kernel_size=1, stride=1, bias=False),
+                norm_layer(planes * expansion),
+            )
+        else:
+            downsample = None
+
+        blocks = list()
+        blocks.append(block_layer(
+            inplanes, planes, stride, downsample, conv_layer, norm_layer, act_layer))
+        inplanes = planes * expansion
+
+        for i in range(1, block_num):
+            blocks.append(block_layer(inplanes, planes, 1, None, conv_layer, norm_layer, act_layer))
+        return nn.Sequential(*blocks)
+
+    def _init_weights(self, zero_init_residual):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _forward_impl(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
---
+++ zcls/model/recognizers/resnet_d_recognizer.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/11/21 下午2:37
+@file: resnet_recognizer.py
+@author: zj
+@description: 
+"""
+
+import torch.nn as nn
+from torch.nn.modules.module import T
+from torchvision.models.utils import load_state_dict_from_url
+
+from .. import registry
+from ..backbones.basicblock import BasicBlock
+from ..backbones.bottleneck import Bottleneck
+from ..backbones.resnet_d_backbone import ResNetDBackbone
+from ..heads.resnet_head import ResNetHead
+from ..norm_helper import get_norm, freezing_bn
+
+arch_settings = {
+    'resnet18': (BasicBlock, (2, 2, 2, 2)),
+    'resnet34': (BasicBlock, (3, 4, 6, 3)),
+    'resnet50': (Bottleneck, (3, 4, 6, 3)),
+    'resnet101': (Bottleneck, (3, 4, 23, 3)),
+    'resnet152': (Bottleneck, (3, 8, 36, 3))
+}
+
+
+class ResNetDRecognizer(nn.Module):
+
+    def __init__(self,
+                 arch='resnet18',
+                 feature_dims=2048,
+                 num_classes=1000,
+                 zcls_pretrained="",
+                 pretrained_num_classes=1000,
+                 fix_bn=False,
+                 partial_bn=False,
+                 norm_layer=None):
+        super(ResNetDRecognizer, self).__init__()
+        assert isinstance(zcls_pretrained, str)
+
+        self.num_classes = num_classes
+        self.fix_bn = fix_bn
+        self.partial_bn = partial_bn
+
+        block_layer, layer_blocks = arch_settings[arch]
+
+        self.backbone = ResNetDBackbone(
+            layer_blocks=layer_blocks,
+            block_layer=block_layer,
+            norm_layer=norm_layer
+        )
+        self.head = ResNetHead(
+            feature_dims=feature_dims,
+            num_classes=pretrained_num_classes
+        )
+
+        self._init_weights(pretrained=zcls_pretrained, pretrained_num_classes=pretrained_num_classes)
+
+    def _init_weights(self, pretrained="", pretrained_num_classes=1000):
+        if pretrained != "":
+            state_dict = load_state_dict_from_url(pretrained, progress=True)
+            self.backbone.load_state_dict(state_dict, strict=False)
+            self.head.load_state_dict(state_dict, strict=False)
+        if self.num_classes != pretrained_num_classes:
+            fc = self.head.fc
+            fc_features = fc.in_features
+            self.head.fc = nn.Linear(fc_features, self.num_classes)
+
+            nn.init.normal_(self.head.fc.weight, 0, 0.01)
+            nn.init.zeros_(self.head.fc.bias)
+
+    def train(self, mode: bool = True) -> T:
+        super(ResNetDRecognizer, self).train(mode=mode)
+
+        if mode and (self.partial_bn or self.fix_bn):
+            freezing_bn(self, partial_bn=self.partial_bn)
+
+        return self
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.head(x)
+
+        return {'probs': x}
+
+
+@registry.RECOGNIZER.register('ResNetD')
+def build_resnet_d(cfg):
+    type = cfg.MODEL.RECOGNIZER.NAME
+    zcls_pretrained = cfg.MODEL.ZClS_PRETRAINED
+    pretrained_num_classes = cfg.MODEL.PRETRAINED_NUM_CLASSES
+    arch = cfg.MODEL.BACKBONE.ARCH
+    num_classes = cfg.MODEL.HEAD.NUM_CLASSES
+    fix_bn = cfg.MODEL.NORM.FIX_BN
+    partial_bn = cfg.MODEL.NORM.PARTIAL_BN
+
+    feature_dims = cfg.MODEL.HEAD.FEATURE_DIMS
+    norm_layer = get_norm(cfg)
+
+    return ResNetDRecognizer(
+        arch=arch,
+        feature_dims=feature_dims,
+        num_classes=num_classes,
+        zcls_pretrained=zcls_pretrained,
+        pretrained_num_classes=pretrained_num_classes,
+        fix_bn=fix_bn,
+        partial_bn=partial_bn,
+        norm_layer=norm_layer
+    )
