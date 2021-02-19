00eb9092e4717823a154d5a0a26ff81a3e728d45
zjkjzj <wy163zhuj@163.com>
2021-02-19 10:15:57 +0800

    refactor(data): refactoring transforms

--- README.md
+++ README.md
@@ -232,7 +232,7 @@ Small note:
 
 * Git submission specifications should be complied with [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0-beta.4/)
 * If versioned, please conform to the [Semantic Versioning 2.0.0](https://semver.org) specification
-* If editing the README, please conform to the[standard-readme](https://github.com/RichardLitt/standard-readme) specification.
+* If editing the README, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
 
 ## License
 
--- zcls/data/transforms/build.py
+++ zcls/data/transforms/build.py
@@ -7,34 +7,54 @@
 @description: 
 """
 
-import torchvision.transforms as transforms
+import ztransforms.cls.transforms as transforms
+import ztransforms.cls.autoaugment as autoaugment
+
+
+def parse_train_transform(cfg):
+    aug_list = list()
+
+    shorter_side = cfg.TRANSFORM.TRAIN.SHORTER_SIDE
+    assert shorter_side > 0
+    aug_list.append(transforms.Resize(shorter_side))
+
+    if cfg.TRANSFORM.TRAIN.CENTER_CROP:
+        crop_size = cfg.TRANSFORM.TRAIN.TRAIN_CROP_SIZE
+        aug_list.append(transforms.CenterCrop(crop_size))
+
+    aug_list.append(transforms.ToTensor())
+
+    MEAN = cfg.TRANSFORM.MEAN
+    STD = cfg.TRANSFORM.STD
+    aug_list.append(transforms.Normalize(MEAN, STD))
+
+    return aug_list
+
+
+def parse_test_transform(cfg):
+    aug_list = list()
+
+    shorter_side = cfg.TRANSFORM.TEST.SHORTER_SIDE
+    assert shorter_side > 0
+    aug_list.append(transforms.Resize(shorter_side))
+
+    if cfg.TRANSFORM.TEST.CENTER_CROP:
+        crop_size = cfg.TRANSFORM.TEST.TEST_CROP_SIZE
+        aug_list.append(transforms.CenterCrop(crop_size))
+
+    aug_list.append(transforms.ToTensor())
+
+    MEAN = cfg.TRANSFORM.MEAN
+    STD = cfg.TRANSFORM.STD
+    aug_list.append(transforms.Normalize(MEAN, STD))
+
+    return aug_list
 
 
 def build_transform(cfg, is_train=True):
-    MEAN = cfg.TRANSFORM.MEAN
-    STD = cfg.TRANSFORM.STD
-
-    aug_list = list()
     if is_train:
-        shorter_side = cfg.TRANSFORM.TRAIN.SHORTER_SIDE
-        assert shorter_side > 0
-        aug_list.append(transforms.Resize(shorter_side))
-        if cfg.TRANSFORM.TRAIN.CENTER_CROP:
-            crop_size = cfg.TRANSFORM.TRAIN.TRAIN_CROP_SIZE
-            aug_list.append(transforms.CenterCrop(crop_size))
-
-        aug_list.append(transforms.ToTensor())
-        aug_list.append(transforms.Normalize(MEAN, STD))
+        aug_list = parse_train_transform(cfg)
     else:
-        shorter_side = cfg.TRANSFORM.TEST.SHORTER_SIDE
-        assert shorter_side > 0
-        aug_list.append(transforms.Resize(shorter_side))
-        if cfg.TRANSFORM.TEST.CENTER_CROP:
-            crop_size = cfg.TRANSFORM.TEST.TEST_CROP_SIZE
-            aug_list.append(transforms.CenterCrop(crop_size))
-
-        aug_list.append(transforms.ToTensor())
-        aug_list.append(transforms.Normalize(MEAN, STD))
-
+        aug_list = parse_test_transform(cfg)
     transform = transforms.Compose(aug_list)
     return transform
