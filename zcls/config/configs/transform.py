9246e4673c0cbad82a70408d1a1dc247d53e45c7
zjkjzj <wy163zhuj@163.com>
2021-02-19 12:14:04 +0800

    feat(transforms): add random_crop/random_horizontal_flip/color_jitter/auto_augment

--- zcls/config/configs/transform.py
+++ zcls/config/configs/transform.py
@@ -8,6 +8,7 @@
 """
 
 from yacs.config import CfgNode as CN
+from ztransforms.cls.autoaugment import AutoAugmentPolicy
 
 
 def add_config(_C):
@@ -18,12 +19,41 @@ def add_config(_C):
     _C.TRANSFORM.MEAN = (0.45, 0.45, 0.45)
     _C.TRANSFORM.STD = (0.225, 0.225, 0.225)
 
+    # ---------------------------------------------------------------------------- #
+    # Train
+    # ---------------------------------------------------------------------------- #
     _C.TRANSFORM.TRAIN = CN()
+
+    # resize
+    _C.TRANSFORM.TRAIN.RESIZE = True
     _C.TRANSFORM.TRAIN.SHORTER_SIDE = 224
+
+    # crop
     _C.TRANSFORM.TRAIN.CENTER_CROP = True
+    _C.TRANSFORM.TRAIN.RANDOM_CROP = False
     _C.TRANSFORM.TRAIN.TRAIN_CROP_SIZE = 224
 
+    # random horizontal flip
+    _C.TRANSFORM.TRAIN.RANDOM_HORIZONTAL_FLIP = False
+
+    # color jitter
+    _C.TRANSFORM.TRAIN.WITH_COLOR_JITTING = False
+    # (brightness, contrast, saturation, hue)
+    _C.TRANSFORM.TRAIN.COLOR_JITTING = (0.1, 0.1, 0.1, 0.1)
+
+    # augo augment
+    _C.TRANSFORM.TRAIN.AUTO_AUGMENT = False
+    _C.TRANSFORM.TRAIN.AUGMENT_POLICY = AutoAugmentPolicy.IMAGENET.value
+
+    # ---------------------------------------------------------------------------- #
+    # Test
+    # ---------------------------------------------------------------------------- #
     _C.TRANSFORM.TEST = CN()
+
+    # resize
+    _C.TRANSFORM.TEST.RESIZE = True
     _C.TRANSFORM.TEST.SHORTER_SIDE = 224
+
+    # crop
     _C.TRANSFORM.TEST.CENTER_CROP = True
     _C.TRANSFORM.TEST.TEST_CROP_SIZE = 224
--- zcls/data/transforms/build.py
+++ zcls/data/transforms/build.py
@@ -14,19 +14,36 @@ import ztransforms.cls.autoaugment as autoaugment
 def parse_train_transform(cfg):
     aug_list = list()
 
-    shorter_side = cfg.TRANSFORM.TRAIN.SHORTER_SIDE
-    assert shorter_side > 0
-    aug_list.append(transforms.Resize(shorter_side))
+    # aug_list.append(transforms.ToPILImage())
+
+    if cfg.TRANSFORM.TRAIN.RESIZE:
+        shorter_side = cfg.TRANSFORM.TRAIN.SHORTER_SIDE
+        assert shorter_side > 0
+        aug_list.append(transforms.Resize(shorter_side))
 
     if cfg.TRANSFORM.TRAIN.CENTER_CROP:
         crop_size = cfg.TRANSFORM.TRAIN.TRAIN_CROP_SIZE
         aug_list.append(transforms.CenterCrop(crop_size))
 
+    if cfg.TRANSFORM.TRAIN.RANDOM_CROP:
+        crop_size = cfg.TRANSFORM.TRAIN.TRAIN_CROP_SIZE
+        aug_list.append(transforms.RandomCrop(crop_size))
+
+    if cfg.TRANSFORM.TRAIN.RANDOM_HORIZONTAL_FLIP:
+        aug_list.append(transforms.RandomHorizontalFlip())
+
+    if cfg.TRANSFORM.TRAIN.WITH_COLOR_JITTING:
+        brightness, contrast, saturation, hue = cfg.TRANSFORM.TRAIN.COLOR_JITTING
+        aug_list.append(
+            transforms.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation, hue=hue))
+
     aug_list.append(transforms.ToTensor())
 
-    MEAN = cfg.TRANSFORM.MEAN
-    STD = cfg.TRANSFORM.STD
-    aug_list.append(transforms.Normalize(MEAN, STD))
+    if cfg.TRANSFORM.TRAIN.AUTO_AUGMENT:
+        policy = cfg.TRANSFORM.TRAIN.AUGMENT_POLICY
+        aug_list.append(autoaugment.AutoAugment(policy=policy))
+
+    aug_list.append(transforms.Normalize(cfg.TRANSFORM.MEAN, cfg.TRANSFORM.STD))
 
     return aug_list
 
@@ -34,9 +51,12 @@ def parse_train_transform(cfg):
 def parse_test_transform(cfg):
     aug_list = list()
 
-    shorter_side = cfg.TRANSFORM.TEST.SHORTER_SIDE
-    assert shorter_side > 0
-    aug_list.append(transforms.Resize(shorter_side))
+    # aug_list.append(transforms.ToPILImage())
+
+    if cfg.TRANSFORM.TEST.RESIZE:
+        shorter_side = cfg.TRANSFORM.TEST.SHORTER_SIDE
+        assert shorter_side > 0
+        aug_list.append(transforms.Resize(shorter_side))
 
     if cfg.TRANSFORM.TEST.CENTER_CROP:
         crop_size = cfg.TRANSFORM.TEST.TEST_CROP_SIZE
@@ -44,9 +64,7 @@ def parse_test_transform(cfg):
 
     aug_list.append(transforms.ToTensor())
 
-    MEAN = cfg.TRANSFORM.MEAN
-    STD = cfg.TRANSFORM.STD
-    aug_list.append(transforms.Normalize(MEAN, STD))
+    aug_list.append(transforms.Normalize(cfg.TRANSFORM.MEAN, cfg.TRANSFORM.STD))
 
     return aug_list
 
