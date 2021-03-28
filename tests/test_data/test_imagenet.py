c546ab480e90c50c2a65c821c2450bfebe53d66c
zjkjzj <wy163zhuj@163.com>
2021-03-28 19:24:17 +0800

    perf(dataset): use lmdb to load imagenet

---
+++ tests/test_data/test_imagenet.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2021/3/28 下午7:11
+@file: test_imagenet.py
+@author: zj
+@description: 
+"""
+
+import cv2
+import numpy as np
+
+from zcls.data.datasets.imagenet import ImageNet
+
+
+def test_imagenet():
+    data_root = 'data/imagenet'
+
+    train_dataset = ImageNet(data_root, train=True)
+    print(train_dataset)
+    print('length:', len(train_dataset))
+
+    img, target = train_dataset.__getitem__(30000)
+    print(type(img), target)
+
+    np_img = np.array(img)
+    print(np_img.shape)
+    cv2.imwrite('test_imagenet.png', np_img)
+
+
+if __name__ == '__main__':
+    test_imagenet()
--- zcls/data/datasets/imagenet.py
+++ zcls/data/datasets/imagenet.py
@@ -7,10 +7,25 @@
 @description: 
 """
 
+import os
+import numpy as np
+from PIL import Image
+import six
+import os.path as osp
+import lmdb
+import pyarrow as pa
 from torch.utils.data import Dataset
 import torchvision.datasets as datasets
 
 from .evaluator.general_evaluator import GeneralEvaluator
+
+
+def loads_pyarrow(buf):
+    """
+    Args:
+        buf: the output of `dumps`.
+    """
+    return pa.deserialize(buf)
 
 
 class ImageNet(Dataset):
@@ -22,18 +37,56 @@ class ImageNet(Dataset):
 
     def __init__(self, root, train=True, transform=None, target_transform=None):
         split = 'train' if train else 'val'
-
-        self.data_set = datasets.ImageNet(root, split=split, transform=transform, target_transform=target_transform)
+        # using torchvision ImageNet to get classes
+        data_set = datasets.ImageNet(root, split=split, transform=transform, target_transform=target_transform)
         self.classes = list()
-        for class_tuple in self.data_set.classes:
+        for class_tuple in data_set.classes:
             self.classes.append(','.join(class_tuple))
+
+        # get dataset
+        self.dbpath = os.path.join(root, f'{split}.lmdb')
+        self.env = lmdb.open(self.dbpath, subdir=osp.isdir(self.dbpath),
+                             readonly=True, lock=False,
+                             readahead=False, meminit=False)
+        with self.env.begin(write=False) as txn:
+            self.length = loads_pyarrow(txn.get(b'__len__'))
+            self.keys = loads_pyarrow(txn.get(b'__keys__'))
+        # get transform and target_transform
+        self.transform = transform
+        self.target_transform = target_transform
+        # create evaluator
         self._update_evaluator()
 
     def __getitem__(self, index: int):
-        return self.data_set.__getitem__(index)
+        env = self.env
+        with env.begin(write=False) as txn:
+            byteflow = txn.get(self.keys[index])
+
+        unpacked = loads_pyarrow(byteflow)
+
+        # load img
+        imgbuf = unpacked[0]
+        buf = six.BytesIO()
+        buf.write(imgbuf)
+        buf.seek(0)
+        img = Image.open(buf).convert('RGB')
+
+        # load label
+        target = unpacked[1]
+
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        # return img, target
+        return img, target
 
     def __len__(self) -> int:
-        return self.data_set.__len__()
+        return self.length
 
     def _update_evaluator(self):
         self.evaluator = GeneralEvaluator(self.classes, topk=(1, 5))
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.dbpath + ')'
