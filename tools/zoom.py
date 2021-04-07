ff38643eeae5db2425a6818f52dc8eb9df6ad1ed
zjkjzj <wy163zhuj@163.com>
2021-04-07 22:49:51 +0800

    perf(zoom): use multiprocess to speed up

--- tools/zoom.py
+++ tools/zoom.py
@@ -17,29 +17,40 @@ root/cat/asd932_.png
 import os
 from ztransforms.cls import Resize
 from PIL import Image
+from multiprocessing import Pool
+
+num_worker = int(os.cpu_count() / 2)
+# Specifies the size of the shorter edge of the image
+short_side = 100
+
+# Dataset source directory
+src_dir = 'data/imagenet/train'
+# Dataset results directory
+dst_dir = 'data/imagenet/train_zoom'
 
 
-def batch_process(model, src_dir, dst_dir):
-    cate_list = os.listdir(src_dir)
-    for i, cate_name in enumerate(cate_list):
-        src_cate_dir = os.path.join(src_dir, cate_name)
-        dst_cate_dir = os.path.join(dst_dir, cate_name)
-        if not os.path.exists(dst_cate_dir):
-            os.makedirs(dst_cate_dir)
+def batch_process(cate_name):
+    # Resize
+    model = get_model(short_side)
+    # for i, cate_name in enumerate(cate_list):
+    src_cate_dir = os.path.join(src_dir, cate_name)
+    dst_cate_dir = os.path.join(dst_dir, cate_name)
+    if not os.path.exists(dst_cate_dir):
+        os.makedirs(dst_cate_dir)
 
-        file_list = os.listdir(src_cate_dir)
-        for file_name in file_list:
-            src_file_path = os.path.join(src_cate_dir, file_name)
-            dst_file_path = os.path.join(dst_cate_dir, file_name)
-            if os.path.isfile(dst_file_path):
-                continue
+    file_list = os.listdir(src_cate_dir)
+    for file_name in file_list:
+        src_file_path = os.path.join(src_cate_dir, file_name)
+        dst_file_path = os.path.join(dst_cate_dir, file_name)
+        if os.path.isfile(dst_file_path):
+            continue
 
-            # fix OSError: cannot write mode RGBA as JPEG
-            src_img = Image.open(src_file_path).convert('RGB')
-            dst_img = model(src_img)
+        # fix OSError: cannot write mode RGBA as JPEG
+        src_img = Image.open(src_file_path).convert('RGB')
+        dst_img = model(src_img)
 
-            dst_img.save(dst_file_path)
-        print(i, src_cate_dir)
+        dst_img.save(dst_file_path)
+    print(src_cate_dir)
 
 
 def get_model(short_side):
@@ -48,19 +59,7 @@ def get_model(short_side):
 
 
 if __name__ == '__main__':
-    # Specifies the size of the shorter edge of the image
-    short_side = 100
+    cate_list = os.listdir(src_dir)
 
-    # Dataset source directory
-    src_dir = 'data/imagenet/train'
-    # Dataset results directory
-    dst_dir = 'data/imagenet/train_zoom'
-    model = get_model(short_side)
-    batch_process(model, src_dir, dst_dir)
-
-    # Dataset source directory
-    src_dir = 'data/imagenet/val'
-    # Dataset results directory
-    dst_dir = 'data/imagenet/val_zoom'
-    model = get_model(short_side)
-    batch_process(model, src_dir, dst_dir)
+    pool = Pool(num_worker)
+    pool.map(batch_process, cate_list)
