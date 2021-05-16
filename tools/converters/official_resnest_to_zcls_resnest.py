978778bf2c8704ed3b9e6983858c43c3f78a624e
zjkjzj <wy163zhuj@163.com>
2021-05-16 19:50:09 +0800

    perf(tools): add resnest50/101/200/269

--- tools/converters/official_resnest_to_zcls_resnest.py
+++ tools/converters/official_resnest_to_zcls_resnest.py
@@ -12,7 +12,7 @@ import torch
 
 from zcls.model.recognizers.build import build_recognizer
 from zcls.util.checkpoint import CheckPointer
-from zcls.config import cfg
+from zcls.config import get_cfg_defaults
 from zcls.model.backbones.resnet.resnet_d_backbone import arch_settings
 
 
@@ -40,9 +40,18 @@ def convert(official_model, zcls_model, stage_repeats):
 def process(item, cfg_file):
     if item == 'resnest50_fast_2s1x64d':
         official_model = torch.hub.load('zhanghang1989/ResNeSt', 'resnest50_fast_2s1x64d', pretrained=True)
+    elif item == 'resnest50':
+        official_model = torch.hub.load('zhanghang1989/ResNeSt', 'resnest50', pretrained=True)
+    elif item == 'resnest101':
+        official_model = torch.hub.load('zhanghang1989/ResNeSt', 'resnest101', pretrained=True)
+    elif item == 'resnest200':
+        official_model = torch.hub.load('zhanghang1989/ResNeSt', 'resnest200', pretrained=True)
+    elif item == 'resnest269':
+        official_model = torch.hub.load('zhanghang1989/ResNeSt', 'resnest269', pretrained=True)
     else:
         raise ValueError(f"{item} doesn't exists")
 
+    cfg = get_cfg_defaults()
     cfg.merge_from_file(cfg_file)
     zcls_model = build_recognizer(cfg, torch.device('cpu'))
 
@@ -57,9 +66,13 @@ def process(item, cfg_file):
 
 
 if __name__ == '__main__':
-    item_list = ['resnest50_fast_2s1x64d', ]
+    item_list = ['resnest50_fast_2s1x64d', 'resnest50', 'resnest101', 'resnest200', 'resnest269']
     cfg_file_list = [
-        'resnest50_fast_2s1x64d_imagenet_224.yaml',
+        'resnest50_fast_2s1x64d_zcls_imagenet_224.yaml',
+        'resnest50_zcls_imagenet_224.yaml',
+        'resnest101_zcls_imagenet_224.yaml',
+        'resnest200_zcls_imagenet_224.yaml',
+        'resnest269_zcls_imagenet_224.yaml',
     ]
     prefix_path = 'configs/benchmarks/resnet'
     for item, cfg_file in zip(item_list, cfg_file_list):
