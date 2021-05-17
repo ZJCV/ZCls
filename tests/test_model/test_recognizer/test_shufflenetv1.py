c5b4f14ff3697122dba56fe6e7f300a964b8134d
zjkjzj <wy163zhuj@163.com>
2021-05-17 15:13:18 +0800

    test(recognizer): shufflenet_v1

---
+++ tests/test_model/test_recognizer/test_shufflenetv1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2021/5/16 下午10:22
+@file: test_shufflenetv1.py
+@author: zj
+@description: 
+"""
+
+import torch
+
+from zcls.config import cfg
+from zcls.config.key_word import KEY_OUTPUT
+from zcls.model.recognizers.build import build_recognizer
+
+
+def test_data(model):
+    data = torch.randn(1, 3, 224, 224)
+    outputs = model(data)[KEY_OUTPUT]
+    print(outputs.shape)
+
+    assert outputs.shape == (1, 1000)
+
+
+def test_shufflenet():
+    cfg.merge_from_file('configs/benchmarks/shufflenet/shufflenet_v1_3g2x_zcls_imagenet_224.yaml')
+    print(cfg)
+    model = build_recognizer(cfg, torch.device('cpu'))
+    print(model)
+
+    test_data(model)
+
+
+if __name__ == '__main__':
+    test_shufflenet()
