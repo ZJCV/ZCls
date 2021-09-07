0f18f07a5d206499c071315d0d243f9d0448f8f8
zjkjzj <wy163zhuj@163.com>
2021-09-07 22:07:30 +0800

    style(square_pad): add note

--- zcls/data/transforms/square_pad.py
+++ zcls/data/transforms/square_pad.py
@@ -7,6 +7,8 @@
 @description: 
 """
 
+import numbers
+
 import numpy as np
 import torchvision.transforms.functional as F
 
@@ -15,12 +17,49 @@ class SquarePad(object):
     """
     By filling the shorter edges, the size of the image becomes square
     refer to [How to resize and pad in a torchvision.transforms.Compose()?](https://discuss.pytorch.org/t/how-to-resize-and-pad-in-a-torchvision-transforms-compose/71850)
+
+    Args:
+        fill (number or str or tuple): Pixel fill value for constant fill. Default is 0. If a tuple of
+            length 3, it is used to fill R, G, B channels respectively.
+            This value is only used when the padding_mode is constant.
+            Only number is supported for torch Tensor.
+            Only int or str or tuple value is supported for PIL Image.
+        padding_mode (str): Type of padding. Should be: constant, edge, reflect or symmetric.
+            Default is constant.
+
+            - constant: pads with a constant value, this value is specified with fill
+
+            - edge: pads with the last value at the edge of the image.
+              If input a 5D torch Tensor, the last 3 dimensions will be padded instead of the last 2
+
+            - reflect: pads with reflection of image without repeating the last value on the edge.
+              For example, padding [1, 2, 3, 4] with 2 elements on both sides in reflect mode
+              will result in [3, 2, 1, 2, 3, 4, 3, 2]
+
+            - symmetric: pads with reflection of image repeating the last value on the edge.
+              For example, padding [1, 2, 3, 4] with 2 elements on both sides in symmetric mode
+              will result in [2, 1, 1, 2, 3, 4, 4, 3]
     """
+
+    def __init__(self, fill=0, padding_mode="constant"):
+        super().__init__()
+
+        if not isinstance(fill, (numbers.Number, str, tuple)):
+            raise TypeError("Got inappropriate fill arg")
+
+        if padding_mode not in ["constant", "edge", "reflect", "symmetric"]:
+            raise ValueError("Padding mode should be either constant, edge, reflect or symmetric")
+        self.fill = fill
+        self.padding_mode = padding_mode
 
     def __call__(self, image):
         w, h = image.size
         max_wh = np.max([w, h])
         hp = int((max_wh - w) / 2)
         vp = int((max_wh - h) / 2)
-        padding = (hp, vp, hp, vp)
-        return F.pad(image, padding, 0, 'constant')
+        padding = [hp, vp, hp, vp]
+        return F.pad(image, padding, self.fill, self.padding_mode)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(fill={0}, padding_mode={1})'. \
+            format(self.fill, self.padding_mode)
