a4e5183eba4cbe23dea90e4e6d8ebcd1ea067a98
zjkjzj <wy163zhuj@163.com>
2021-09-23 22:15:45 +0800

    feat(dataset): add GeneralDatasetV2

--- zcls/data/datasets/build.py
+++ zcls/data/datasets/build.py
@@ -11,6 +11,7 @@ from .cifar import CIFAR
 from .fashionmnist import FashionMNIST
 from .imagenet import ImageNet
 from .general_dataset import GeneralDataset
+from .general_dataset_v2 import GeneralDatasetV2
 from .lmdb_dataset import LMDBDataset
 from .lmdb_imagenet import LMDBImageNet
 
@@ -34,6 +35,8 @@ def build_dataset(cfg, transform=None, target_transform=None, is_train=True):
                            top_k=top_k)
     elif dataset_name == 'GeneralDataset':
         dataset = GeneralDataset(data_root, transform=transform, target_transform=target_transform, top_k=top_k)
+    elif dataset_name == 'GeneralDatasetV2':
+        dataset = GeneralDatasetV2(data_root, transform=transform, target_transform=target_transform, top_k=top_k)
     elif dataset_name == 'LMDBDataset':
         dataset = LMDBDataset(data_root, transform=transform, target_transform=target_transform, top_k=top_k)
     elif dataset_name == 'LMDBImageNet':
---
+++ zcls/data/datasets/general_dataset_v2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2021/9/23 下午10:06
+@file: general_dataset_v2.py
+@author: zj
+@description: 
+"""
+
+import os
+import json
+
+from PIL import Image
+
+from torch.utils.data import Dataset
+from .evaluator.general_evaluator import GeneralEvaluator
+
+
+class GeneralDatasetV2(Dataset):
+
+    def __init__(self, root, transform=None, target_transform=None, top_k=(1, 5)):
+        assert os.path.isfile(root)
+        with open(root, 'r') as f:
+            data_dict = json.load(f)
+
+        self.classes = list(data_dict.keys())
+        self.total_img_list = list()
+        self.total_label_list = list()
+        for key in self.classes:
+            img_list = data_dict[key]
+
+            label = self.classes.index(key)
+            for img_path in img_list:
+                assert os.path.isfile(img_path), img_path
+
+                self.total_img_list.append(img_path)
+                self.total_label_list.append(label)
+
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+        self._update_evaluator(top_k)
+
+    def __getitem__(self, index: int):
+        img_path = self.total_img_list[index]
+        target = self.total_label_list[index]
+
+        image = Image.open(img_path)
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return image, target
+
+    def __len__(self) -> int:
+        return len(self.total_img_list)
+
+    def _update_evaluator(self, top_k):
+        self.evaluator = GeneralEvaluator(self.classes, top_k=top_k)
+
+    def get_classes(self):
+        return self.classes
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.root + ')'
