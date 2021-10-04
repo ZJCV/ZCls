35b8b0bcbee7b3bb02716f65898152b62d2812cf
zjkjzj <wy163zhuj@163.com>
2021-10-04 10:37:40 +0800

    test(transforms): albumentation vs. torchvision

---
+++ tests/data/transforms/albumentation_vs_torchvision.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2021/10/4 上午10:24
+@file: albumentation_vs_torchvision.py
+@author: zj
+@description: 
+"""
+
+import cv2
+import time
+
+import numpy as np
+from PIL import Image
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
+
+def get_torchvision_transform():
+    return transforms.Compose([
+        transforms.RandomRotation((-30, 30)),
+        transforms.Resize((256, 256)),
+        transforms.RandomCrop((224, 224)),
+        transforms.RandomHorizontalFlip(p=1.0),
+        transforms.ColorJitter(brightness=0.2, contrast=0., saturation=0., hue=0.),
+        transforms.ToTensor(),
+        transforms.RandomErasing(),
+        transforms.Normalize([0.445, 0.445, 0.445], [0.225, 0.225, 0.225]),
+    ])
+
+
+def get_albumentation_transform():
+    # Declare an augmentation pipeline
+    return A.Compose([
+        A.Rotate(limit=(-30, 30), interpolation=cv2.INTER_LINEAR, p=1.0),
+        A.Resize(256, 256, interpolation=cv2.INTER_LINEAR, p=1.0),
+        A.RandomCrop(width=224, height=224, p=1.0),
+        A.HorizontalFlip(p=1.0),
+        A.ColorJitter(brightness=0.2, contrast=0., saturation=0., p=1.0),
+        A.CoarseDropout(max_height=16, max_width=16, fill_value=0, p=1.0),
+        A.Normalize(mean=[0.445, 0.445, 0.445], std=[0.225, 0.225, 0.225], max_pixel_value=255.0, p=1.0),
+        ToTensorV2(p=1.0),
+    ])
+
+
+def compute_time(transform, is_pil=False):
+    t = 0.
+    for i in tqdm(range(1000)):
+        data = np.random.randn(224, 224, 3).astype(np.uint8)
+        if is_pil:
+            data = Image.fromarray(data)
+
+        t1 = time.time()
+        if is_pil:
+            transform(data)
+        else:
+            transform(image=data)
+        t += time.time() - t1
+
+    print('average time is:', (t / 1000))
+
+
+if __name__ == '__main__':
+    t = get_torchvision_transform()
+    compute_time(t, is_pil=True)
+
+    t = get_albumentation_transform()
+    compute_time(t, is_pil=False)
