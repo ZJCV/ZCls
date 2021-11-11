16842cc140f540ae4d2d73a4e17d2b89b5ee62b6
zjkjzj <wy163zhuj@163.com>
2021-11-11 20:46:35 +0800

    refactor(key_word.py): add annotation for key word

--- zcls/config/key_word.py
+++ zcls/config/key_word.py
@@ -7,9 +7,17 @@
 @description: 
 """
 
+# Output identifier
 KEY_OUTPUT = 'output'
+# Loss identifier
 KEY_LOSS = 'loss'
 
+# Separator for csv file
 KEY_SEP = ',,'
+# Data file name is used for MPDataset
 KEY_DATASET = 'data.csv'
+# Class file name is used for MPDataset
 KEY_CLASSES = 'cls.csv'
+
+# empty identifier
+KEY_EMPTY = ""
