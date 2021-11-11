90ef2f04c6715d54a00adae44596dda780726028
zjkjzj <wy163zhuj@163.com>
2021-11-11 22:28:27 +0800

    style(shufflenet): add shufflenet_v1/v2's url_map

--- zcls/model/recognizers/shufflenet/shufflenetv1.py
+++ zcls/model/recognizers/shufflenet/shufflenetv1.py
@@ -8,6 +8,7 @@
 """
 
 from zcls.model import registry
+from zcls.model.misc import load_pretrained_weights
 from ..base_recognizer import BaseRecognizer
 
 """
@@ -15,9 +16,31 @@ Note 1: Empirically g = 3 usually has a proper trade-off between accuracy and ac
 Note 2: Comparing ShuffleNet 2× with MobileNet whose complexity are comparable (524 vs. 569 MFLOPs)
 """
 
+url_map = {
+    'shufflenetv1_3g0_5x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_3g0_5x_imagenet.pth",
+    'shufflenetv1_3g1_5x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_3g1_5x_imagenet.pth",
+    'shufflenetv1_3g1x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_3g1x_imagenet.pth",
+    'shufflenetv1_3g2x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_3g2x_imagenet.pth",
+    'shufflenetv1_8g0_5x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_8g0_5x_imagenet.pth",
+    'shufflenetv1_8g1_5x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_8g1_5x_imagenet.pth",
+    'shufflenetv1_8g1x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_8g1x_imagenet.pth",
+    'shufflenetv1_8g2x': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenetv1_8g2x_imagenet.pth",
+}
+
 
 @registry.RECOGNIZER.register('ShuffleNetV1')
 class ShuffleNetV1(BaseRecognizer):
 
     def __init__(self, cfg):
         super().__init__(cfg)
+
+    def _init_weights(self, cfg):
+        pretrained_local = cfg.MODEL.RECOGNIZER.PRETRAINED_LOCAL
+        pretrained_num_classes = cfg.MODEL.RECOGNIZER.PRETRAINED_NUM_CLASSES
+        num_classes = cfg.MODEL.HEAD.NUM_CLASSES
+        load_pretrained_weights(self, cfg.MODEL.BACKBONE.ARCH,
+                                weights_path=None if pretrained_local == '' else pretrained_local,
+                                load_fc=pretrained_num_classes == num_classes,
+                                verbose=True,
+                                url_map=url_map if cfg.MODEL.RECOGNIZER.PRETRAINED_REMOTE else None
+                                )
--- zcls/model/recognizers/shufflenet/shufflenetv2.py
+++ zcls/model/recognizers/shufflenet/shufflenetv2.py
@@ -8,6 +8,7 @@
 """
 
 from zcls.model import registry
+from zcls.model.misc import load_pretrained_weights
 from ..base_recognizer import BaseRecognizer
 
 """
@@ -17,9 +18,27 @@ G3) Network fragmentation reduces degree of parallelism
 G4) Element-wise operations are non-negligible.
 """
 
+url_map = {
+    'shufflenet_v2_x0_5': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenet_v2_x0_5_imagenet.pth",
+    'shufflenet_v2_x1_0': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenet_v2_x1_0_imagenet.pth",
+    'shufflenet_v2_x1_5': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenet_v2_x1_5_imagenet.pth",
+    'shufflenet_v2_x2_0': "https://github.com/ZJCV/ZCls/releases/download/v0.14.0/shufflenet_v2_x2_0_imagenet.pth",
+}
+
 
 @registry.RECOGNIZER.register('ShuffleNetV2')
 class ShuffleNetV2(BaseRecognizer):
 
     def __init__(self, cfg):
         super().__init__(cfg)
+
+    def _init_weights(self, cfg):
+        pretrained_local = cfg.MODEL.RECOGNIZER.PRETRAINED_LOCAL
+        pretrained_num_classes = cfg.MODEL.RECOGNIZER.PRETRAINED_NUM_CLASSES
+        num_classes = cfg.MODEL.HEAD.NUM_CLASSES
+        load_pretrained_weights(self, cfg.MODEL.BACKBONE.ARCH,
+                                weights_path=None if pretrained_local == '' else pretrained_local,
+                                load_fc=pretrained_num_classes == num_classes,
+                                verbose=True,
+                                url_map=url_map if cfg.MODEL.RECOGNIZER.PRETRAINED_REMOTE else None
+                                )
